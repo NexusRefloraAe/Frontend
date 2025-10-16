bf40b99f5f0a8a0a0fdac3c80be83f8395115930
edsonplz <edson.silva@dcx.ufpb.br>
2025-10-16 15:38:59 -0300

    Quase finalizado

--- front-reflora/src/pages/Banco/Banco.jsx
+++ front-reflora/src/pages/Banco/Banco.jsx
@@ -5,6 +5,7 @@ import listar from '../../assets/listar.png'
 import arrows from '../../assets/arrows-up-down.svg'
 import search from '../../assets/search.svg'
 import bell from '../../assets/bell.svg'
+import share from '../../assets/Share.svg'
 
 function Banco() {
     return (
@@ -17,7 +18,7 @@ function Banco() {
                 <nav className="bottons-banco">
                     <ul>
                         <li><button><img src={cadastrar} alt="Cadastrar Semente" />Cadastrar Semente</button></li>
-                        <li><button><img src={listar} alt="Listar Sementes" />Listar Sementes</button></li>
+                        <li className='button-active'><button><img src={listar} alt="Listar Sementes"/>Listar Sementes</button></li>
                     </ul>
                 </nav>
                 <main>
@@ -32,62 +33,107 @@ function Banco() {
                         <div className="infos-sementes-card">
                             <table>
                                 <tr>
-                                    <th>Lote   <img src={arrows} alt="Ordenar"/></th>
-                                    <th>Data Cadastro    <img src={arrows} alt="Ordenar"/></th>
-                                    <th>Nome popular    <img src={arrows} alt="Ordenar"/></th>
-                                    <th>Quantidade Atual <img src={arrows} alt="Ordenar"/></th>
+                                    <th>Lote   <img src={arrows} alt="Ordenar" /></th>
+                                    <th>Data Cadastro    <img src={arrows} alt="Ordenar" /></th>
+                                    <th>Nome popular    <img src={arrows} alt="Ordenar" /></th>
+                                    <th>Quantidade Atual <img src={arrows} alt="Ordenar" /></th>
                                     <th>Quantidade Saida</th>
                                     <th>Finalidade</th>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                                 <tr>
                                     <td>A001</td>
                                     <td>xx/xx/xxxx</td>
-                                    <td>Ipê-amarelo</td>
+                                    <td><a href="">Ipê-amarelo</a></td>
                                     <td>2000 kg</td>
-                                    <td>200 g</td>
-                                    <td>Teste de germinação</td>
+                                    <td><input type="text" /></td>
+                                    <td><select>
+                                        <option value="germinacao">Teste de germinação</option>
+                                        <option value="plantio">Teste de plantio</option>
+                                        <option value="colheita">Teste de colheita</option>
+                                    </select>
+                                    </td>
                                 </tr>
                             </table>
+                        </div>
+                        <div className="footer-content">
+                            <nav>
+                                <ul>
+                                    <li><a href="#">Previous</a></li>
+                                    <li><a href="#" className='active'><span>1</span></a></li>
+                                    <li><a href="#">2</a></li>
+                                    <li><a href="#">3</a></li>
+                                    <li><a href="#">...</a></li>
+                                    <li><a href="#">67</a></li>
+                                    <li><a href="#">68</a></li>
+                                    <li><a href="#">Next</a></li>
+                                </ul>
+                            </nav>
+                            <button>Exportar <img src={share} alt="" /></button>
                         </div>
                     </section>
                 </main>
