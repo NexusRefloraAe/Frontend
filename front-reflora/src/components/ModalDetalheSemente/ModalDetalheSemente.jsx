d1141f9da325ea1dad788b990bf606e29335f009
edsonplz <edson.silva@dcx.ufpb.br>
2025-11-06 11:37:42 -0300

    Tabela historico adicionado

--- front-reflora/src/components/ModalDetalheSemente/ModalDetalheSemente.jsx
+++ front-reflora/src/components/ModalDetalheSemente/ModalDetalheSemente.jsx
@@ -2,11 +2,28 @@ import React, { useState, useEffect, use } from 'react'
 import './ModalDetalheSemente.css'
 import Paginacao from '../Paginacao/Paginacao'
 import Button from '../Button/Button'
+import TabelaHistorico from '../TabelaHistorico/TabelaHistorico'
 //Icons
 import closeIcon from '../../assets/close.svg'
 import editIcon from '../../assets/edit.svg'
 import deleteIcon from '../../assets/delete.svg'
 import shareIcon from '../../assets/Share.svg'
+
+const colunasEntrada = [
+    { titulo: 'Lote', chave: 'lote', sortable: true },
+    { titulo: 'Data', chave: 'data', sortable: true },
+    { titulo: 'Nome Popular', chave: 'nome', sortable: true },
+    { titulo: 'Quantidade (kg)', chave: 'qtd', sortable: true },
+    { titulo: 'Câmara Fria', chave: 'camaraFria', sortable: true },
+];
+
+const colunasSaida = [
+    { titulo: 'Lote', chave: 'lote', sortable: true },
+    { titulo: 'Data', chave: 'data', sortable: true },
+    { titulo: 'Nome Popular', chave: 'nome', sortable: true },
+    { titulo: 'Quantidade (kg)', chave: 'qtd', sortable: true },
+    { titulo: 'Câmara Fria', chave: 'camaraFria', sortable: true },
+];
 
 /**
  * @param {object} props
@@ -24,12 +41,28 @@ function ModalDetalheSemente({ semente, onClose }) {
         setHistoricoEntrada([
             { lote: 'A001', data: '20/05/2025', nome: 'Eucalipito', qtd: 2000, camaraFria: 'Sim' },
             { lote: 'A002', data: '15/06/2025', nome: 'Pau-Brasil', qtd: 1500, camaraFria: 'Não' },
+            { lote: 'A003', data: '18/06/2025', nome: 'Ipê-amarelo', qtd: 1000, camaraFria: 'Sim' },
+            { lote: 'A004', data: '25/06/2025', nome: 'Jacarandá', qtd: 800, camaraFria: 'Não' },
+            { lote: 'A005', data: '30/06/2025', nome: 'Copaíba', qtd: 600, camaraFria: 'Sim' },
+            { lote: 'A006', data: '05/07/2025', nome: 'Barbatimão', qtd: 400, camaraFria: 'Não' },
         ])
         setHistoricoSaida([
-            { lote: 'A001', data: '10/07/2025', nome: 'Eucalipito', qtd: 500, finalidade: 'Germinação' },
-            { lote: 'A002', data: '12/08/2025', nome: 'Pau-Brasil', qtd: 300, finalidade: 'Plantio' },
+            { lote: 'A001', data: '10/07/2025', nome: 'Eucalipito', qtd: 500, camaraFria: 'Sim' },
+            { lote: 'A002', data: '12/08/2025', nome: 'Pau-Brasil', qtd: 300, camaraFria: 'Não' },
         ])
     }, [semente.id]);
+
+    const ITENS_POR_PAGINA = 2;
+
+    const totalItens = Math.max(historicoEntrada.length, historicoSaida.length);
+    const totalPaginas = Math.ceil(totalItens / ITENS_POR_PAGINA);
+
+    const indiceUltimo = paginaHistorico * ITENS_POR_PAGINA;
+    const indicePrimeiro = indiceUltimo - ITENS_POR_PAGINA;
+
+    const entradasPagina = historicoEntrada.slice(indicePrimeiro, indiceUltimo);
+    const saidasPagina = historicoSaida.slice(indicePrimeiro, indiceUltimo);
+
     return (
         <div className='modal-overlay' onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
@@ -64,14 +97,24 @@ function ModalDetalheSemente({ semente, onClose }) {
                     <div className="historico-tabelas">
                         <div className="tabela-wrapper">
                             <h4 className='tabela-entrada'>Entradas</h4>
+                            <TabelaHistorico
+                                colunas={colunasEntrada}
+                                dados={entradasPagina}
+                                variant="entrada"
+                            />
                         </div>
                         <div className="tabela-wrapper">
                             <h4 className='tabela-saida'>Saídas</h4>
+                            <TabelaHistorico
+                                colunas={colunasSaida}
+                                dados={saidasPagina}
+                                variant="saida"
+                            />
                         </div>
                     </div>
 
                     <div className="footer-content">
-                        <Paginacao paginaAtual={paginaHistorico} totalPaginas={3} onPaginaChange={setPaginaHistorico} />
+                        <Paginacao paginaAtual={paginaHistorico} totalPaginas={totalPaginas} onPaginaChange={setPaginaHistorico} />
                         <Button variant='primary' icon={shareIcon}>Exportar</Button>
                     </div>
                 </div>
